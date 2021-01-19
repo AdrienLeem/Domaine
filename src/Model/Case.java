1152b61badfbd7e9d4201bb33f7e0114123c318d
AdrienLeem <adrienleem@gmail.com>
2021-01-19 12:18:48 +0100

    Fix Case

--- src/Model/Case.java
+++ src/Model/Case.java
@@ -4,11 +4,19 @@ public abstract class Case {
     private int x;
     private int y;
     private int valeur;
+    private boolean fNord;
+    private boolean fSud;
+    private boolean fEst;
+    private boolean fOuest;
 
     public Case(int x, int y,int valeur) {
         this.x = x;
         this.y = y;
         this.valeur = valeur;
+        this.fNord = false;
+        this.fSud = false;
+        this.fEst = false;
+        this.fOuest = false;
     }
 
     public int getX() {
@@ -21,5 +29,37 @@ public abstract class Case {
 
     public int getValeur() {
         return valeur;
+    }
+
+    public boolean isfNord() {
+        return fNord;
+    }
+
+    public void setfNord(boolean fNord) {
+        this.fNord = fNord;
+    }
+
+    public boolean isfSud() {
+        return fSud;
+    }
+
+    public void setfSud(boolean fSud) {
+        this.fSud = fSud;
+    }
+
+    public boolean isfEst() {
+        return fEst;
+    }
+
+    public void setfEst(boolean fEst) {
+        this.fEst = fEst;
+    }
+
+    public boolean isfOuest() {
+        return fOuest;
+    }
+
+    public void setfOuest(boolean fOuest) {
+        this.fOuest = fOuest;
     }
 }
